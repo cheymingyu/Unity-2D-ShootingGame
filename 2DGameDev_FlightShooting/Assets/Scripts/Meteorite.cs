ffc0224c733637bf2433e6bf162b7b269cf51e92
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-04 22:06:56 +0900

    Update explosion effect

--- 2DGameDev_FlightShooting/Assets/Scripts/Meteorite.cs
+++ 2DGameDev_FlightShooting/Assets/Scripts/Meteorite.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class Meteorite : MonoBehaviour
 {
     [SerializeField]
-    private int damage = 5;        // 운석 공격력
+    private int damage = 5;                     // 운석 공격력
+    [SerializeField]
+    private GameObject explosionPrefab;         // 폭발 효과
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // 운석에게 부딪힌 오브젝트의 태그가 "Player"이면
@@ -11,6 +14,8 @@ public class Meteorite : MonoBehaviour
         {
             // 운석 공격력만큼 플레이어 체력 감소
             collision.GetComponent<PlayerHP>().TakeDamage(damage);
+            // 폭발 이펙트 생성
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             // 운석 사망
             Destroy(gameObject);
         }
