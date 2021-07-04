e8aee652fc88310573ba0675a4930a45533b3421
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-04 21:46:24 +0900

    Update PlayerController.cs to update die statement

--- 2DGameDev_FlightShooting/Assets/Scripts/PlayerController.cs
+++ 2DGameDev_FlightShooting/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,15 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private string nextSceneName;
+    private string      nextSceneName;
     [SerializeField]
-    private StageData stageData;
+    private StageData   stageData;
     [SerializeField]
-    private KeyCode KeyCodeAttack = KeyCode.Space;
-    private Movement2D movement2D;
-    private Weapon weapon;
+    private KeyCode     KeyCodeAttack = KeyCode.Space;
+    private bool        isDie = false;
+    private Movement2D  movement2D;
+    private Weapon      weapon;
+    private Animator    animator;
 
     private int score;
     public int Score
@@ -24,10 +26,14 @@ public class PlayerController : MonoBehaviour
     {
         movement2D  = GetComponent<Movement2D>();
         weapon      = GetComponent<Weapon>();
+        animator    = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        // 플레이어가 사망 애니메이션 재생 중일 때 이동, 공격이 불가능하게 설정
+        if (isDie == true) return;
+
         // 방향 키를 눌러 이동 방향 설정
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
@@ -53,6 +59,18 @@ public class PlayerController : MonoBehaviour
     }
 
     public void OnDie()
+    {
+        // 이동 방향 초기화
+        movement2D.MoveTo(Vector3.zero);
+        // 사망 애니메이션 재생
+        animator.SetTrigger("onDie");
+        // 적들과 충돌하지 않도록 충돌 박스 삭제
+        Destroy(GetComponent<CircleCollider2D>());
+        // 사망 시 키 플레이어 조작 등을 하지 못하게 하는 변수
+        isDie = true;
+    }
+
+    public void OnDieEvent()
     {
         // 디바이스에 획득한 점수 score 저장
         PlayerPrefs.SetInt("Score", score);
