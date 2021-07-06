ea13c0b537131823e9c2baa898966b7e9ab08585
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-06 16:31:47 +0900

    Update BossHPViewer.cs

--- 2DGameDev_FlightShooting/Assets/Scripts/BossHPViewer.cs
+++ 2DGameDev_FlightShooting/Assets/Scripts/BossHPViewer.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BossHPVIewer : MonoBehaviour
+public class BossHPViewer : MonoBehaviour
 {
     [SerializeField]
     private BossHP bossHP;
